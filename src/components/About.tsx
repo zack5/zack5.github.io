08dd3b9ad42e07d5f5842fcb01475b54eff57623
zack5 <zack5@users.noreply.github.com>
2025-03-09 22:03:37 -0400

    Wording changes to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,8 @@ export default function About() {
   return (
     <div className="about">
       <h2>About</h2>
-      <p>I'm a programmer who loves creating organized and intuitive user experiences. I have five years of experience building interfaces in multiplayer, multiplatform video games.</p>
-      <p>I graduated from Stanford University in 2020 with MS and BS degrees in Computer Science. My resume is available <a href="/Zack Cinquini 2025.pdf">here</a>. Please feel free to reach out!</p>
+      <p>I'm a programmer who loves creating organized and intuitive user experiences. For the past five years, I've been building interfaces in multiplayer, multiplatform video games.</p>
+      <p>I graduated from Stanford University in 2020 with MS and BS degrees in Computer Science. Please feel free to look at my <a href="/Zack Cinquini 2025.pdf">resume</a> or to reach out!</p>
     </div>
   );
 }
